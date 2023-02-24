7513e314382a4382147a76e5243a1cd4cd43825f
ifty17 <iftyrahatkabir@gmail.com>
2023-02-24 09:35:43 +0600

    redux

--- src/redux/counter/actionTypes.js
+++ src/redux/counter/actionTypes.js
@@ -1,2 +1,5 @@
-export const INCREMENET = "counter/increment";
+export const INCREMENT = "counter/increment";
 export const DECREMENT = "counter/decrement";
+
+//export const INCREMENT = 'counter/increment'
+//export const decrement = 'counter/decrement'
--- src/redux/counter/actions.js
+++ src/redux/counter/actions.js
@@ -1,15 +1,30 @@
-import { DECREMENT, INCREMENET } from "./actionTypes";
+// import { DECREMENT, INCREMENET } from "./actionTypes";
 
-export const increment = (value) => {
-    return {
-        type: INCREMENET,
-        payload: value,
-    };
+// export const increment = (value) => {
+//     return {
+//         type: INCREMENET,
+//         payload: value,
+//     };
+// };
+
+// export const decrement = (value) => {
+//     return {
+//         type: DECREMENT,
+//         payload: value,
+//     };
+// };
+
+import { INCREMENT, DECREMENT } from "./actionTypes"
+
+export const increment = (value) =>{
+    return{
+        type: INCREMENT, 
+        payload: value
+    }
 };
-
-export const decrement = (value) => {
-    return {
-        type: DECREMENT,
-        payload: value,
-    };
+export const decrement = (value) =>{
+    return{
+        type: DECREMENT, 
+        payload: value
+    }
 };
